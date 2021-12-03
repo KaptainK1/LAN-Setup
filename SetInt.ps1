250559468b7b79d57495ee4fdc346a706f50873c
Dylan Hoffman <jkioicefire.gmail.com>
2021-12-03 12:19:26 -0600

    updated powershell script to check for wifi vs ethernet

--- SetInt.ps1
+++ SetInt.ps1
@@ -5,9 +5,16 @@ $zInt = Get-NetIPInterface | Where-Object {$_.InterfaceAlias -Match "ZeroTier*"}
 $zNum = $zInt.Get(0).ifIndex
 Write-Output $zNum
 
+$eNum
 $eInt = Get-NetIPInterface | Where-Object {$_.InterfaceAlias -Match "Ethernet"}
-$eNum = $eInt.Get(0).ifIndex
-Write-Output $eNum
+if($eInt -eq $null) {
+    $wInt = Get-NetIPInterface | Where-Object {$_.InterfaceAlias -Match "Wi-Fi*"}
+    $eNum = $wInt.Get(0).ifIndex
+} else {
+    $eNum = $eInt.Get(0).ifIndex
+    Write-Output $eNum
+}
+
 
 Set-NetIPInterface -InterfaceIndex $zNum -InterfaceMetric $zIntMetric
 Set-NetIPInterface -InterfaceIndex $eNum -InterfaceMetric $eIntMetric
