aaac53052a898dd328f7688b28dbec1d3753c66a
Dylan Hoffman <jkioicefire.gmail.com>
2021-12-03 11:28:00 -0600

    added powershell script and instructions

---
+++ SetInt.ps1
@@ -0,0 +1,8 @@
+﻿param ([UInt32]$zeroTierInt, [Int32]$ethernetInt)
+
+Write-Output $zeroTierInt
+Write-Output $ethernetInt
+
+Set-NetIPInterface -InterfaceIndex $zeroTierInt -InterfaceMetric 80
+Set-NetIPInterface -InterfaceIndex $ethernetInt -InterfaceMetric 100
+Get-NetIPInterface
